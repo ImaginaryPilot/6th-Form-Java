526572aa94a849521df22b1942b294805216cb4f
ImaginaryPilot <107716532+ImaginaryPilot@users.noreply.github.com>
2023-01-11 09:23:12 +0100

    Create WhatsTheWeather.java

---
+++ project-curiosity/src/lesson06/WhatsTheWeather.java
@@ -0,0 +1,18 @@
+package lesson06;
+
+public class WhatsTheWeather {
+
+    // let us agree on a number chart
+    final int Sunny = 0;
+    final int Cloudy = 1;
+    final int Rain = 2;
+    final int Snow = 3;
+    final int Thunder = 4;
+
+    // Arrays
+    char[] date = {'D', 'M', 'T'};
+
+    public static void main(String[] args){
+
+    }
+}
