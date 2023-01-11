e2f47811d317a7649183743b47b51e26a23a0fac
ImaginaryPilot <107716532+ImaginaryPilot@users.noreply.github.com>
2023-01-11 11:39:00 +0100

    Fridge Engineer

--- project-curiosity/src/lesson07/Soldier.java
+++ project-curiosity/src/lesson07/Soldier.java
@@ -2,4 +2,31 @@ package lesson07;
 
 public class Soldier {
     
+    /*
+     * ------- Attributes -------
+     * These are all the properties that describe the state.
+     */
+    int health;
+    String name;
+    int ID;
+    String hungerLevel = "Borgor";
+    boolean isHungry; 
+
+    /*
+     * ------- Coming Soon -------
+     */
+
+    /*
+     * ------- Behavioural Methods -------
+     * The functions an object can use to update the state.
+     */
+
+     public void damage() {
+        health -= 1;
+     }
+
+     public void eat() {
+        isHungry = false;
+        hungerLevel = "Hamborgor";
+     }
 }
