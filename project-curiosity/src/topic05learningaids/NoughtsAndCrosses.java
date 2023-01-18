c0d6bd6e590cf6fd76d59e278d8a32fb03c07756
ImaginaryPilot <107716532+ImaginaryPilot@users.noreply.github.com>
2023-01-18 10:03:19 +0100

    Update NoughtsAndCrosses.java

--- project-curiosity/src/topic05learningaids/NoughtsAndCrosses.java
+++ project-curiosity/src/topic05learningaids/NoughtsAndCrosses.java
@@ -7,13 +7,38 @@ package topic05learningaids;
 public class NoughtsAndCrosses {
 
     // TODO: declare char 2D array here
+    static char[][] grid;
     
-    public static void main() {
+    public static void main(String[] args) {
+        // allocate memory for my grid
+        grid = new char[3][3];
+        displayGrid();
 
+        // Filling the first row with crosses
+        grid[0][0] = 'X';
+        grid[0][1] = 'X';
+        grid[0][2] = 'X';
+
+        displayGrid();
     }
 
     public static void displayGrid() {
-        // TODO
+        // key lesson is navigating through the 2D array
+        // using nested for loops
+        for (int row = 0; row < grid.length; row++) { // jump to row (vertically)
+            System.out.println("---------------");
+            for (int col = 0; col < grid[0].length; col++) { // jump to col (horizontally)
+                if(col == 0){
+                    System.out.print("  ");
+                }
+                if(col == 0){
+                    System.out.print("  ");
+                } 
+                System.out.print(grid[row][col] + "  |  ");
+            }
+            System.out.println("");
+        }
+        System.out.println("---------------");
     }
 
     public static boolean horizontalCheck(int row, char symbol) {
