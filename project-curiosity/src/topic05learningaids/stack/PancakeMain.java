d4558ae6437f4ee7bf1123e3a2bdf85ccaba2325
ImaginaryPilot <107716532+ImaginaryPilot@users.noreply.github.com>
2023-03-22 08:57:57 +0100

    Pancake

--- project-curiosity/src/topic05learningaids/stack/PancakeMain.java
+++ project-curiosity/src/topic05learningaids/stack/PancakeMain.java
@@ -1,8 +1,42 @@
 package topic05learningaids.stack;
 
+import helpers.Keyboard;
+
 public class PancakeMain {
     public static void main(String[] args){
         // instantiate a PancakeStack
         PancakeStack myBreakfast = new PancakeStack("Nutella");
+
+        // ask the user how many pancakes they want, must be between 2 and 5
+        System.out.println("Please enter the number of pancakes(must be between 2 and 5): ");
+        int numberOfPancakes = Keyboard.readInt();
+        if(numberOfPancakes < 2 || numberOfPancakes > 5){
+            System.out.println("Please re-enter the number of pancakes(must be between 2 and 5): ");
+            numberOfPancakes = Keyboard.readInt();
+        }
+
+        // loop between 0 up to not including number, and push
+        for (int i = 0; i < numberOfPancakes; i++){
+            myBreakfast.push();
+        }
+
+        // Confirm to user that pancake stack is ready to eat
+        System.out.println("Your pancake stack is ready to eat!");
+
+        char answer = 'y';
+        do {
+            // ask the user if they now want to eat the pancake
+            System.out.println("Do you wish to eat a pancake? (y/n): ");
+            answer = Keyboard.readChar();
+            if(answer == 'y') {
+                System.out.println("How many do you want to eat? ");
+                Pancake eatMe = myBreakfast.pop();
+            } else{
+                System.out.println("Well the pancake is going to get cold...");
+            }
+    
+        } while(answer == 'y' && !myBreakfast.isEmpty());
+
+        System.out.println("Adios compadre, hasta la vista");
     }
 }
--- project-curiosity/src/topic05learningaids/stack/PancakeStack.java
+++ project-curiosity/src/topic05learningaids/stack/PancakeStack.java
@@ -2,44 +2,54 @@ package topic05learningaids.stack;
 
 import java.util.Stack;
 
+/**
+ * 5.1.10 Arrays as static stacks
+ * "As with any abstract data type, 
+ *  a stack can be implemented with a variety of data structures, 
+ *  such as a linked list or an array. A stack has a variety of applications 
+ *  such as in reversing the order of elements, evaluating polish strings, etc."
+ * https://brilliant.org/wiki/stacks/
+ */
 public class PancakeStack {
-    /**
-     * Atrributes (State)
-     */
 
+    /**
+     * --- Attributes (State) -----------------------------
+     */
     int size; // this is a stack attribute
 
-    Pancake[] stack = new Pancake[5];
+    Pancake[] stack = new Pancake[7];
 
     String topping; // this is a pancake attribute
 
-    public PancakeStack(String topping){
+    public PancakeStack(String topping) {
         this.topping = topping;
-
+        this.size = 0;
     }
 
-    /** 
-     * Method (Behavior)
+    /**
+     * --- Methods (Behaviour) -----------------------------
      */
-    public void push(){
-        if (isFull()) return;
-        // create the pancake
-        Pancake p = new Pancake();
-        int pos = stack.length - size;
-        stack[pos]= p;
-        size++;
+    public void push() {
+        if (!isFull()) {
+            // create the pancake
+            Pancake p = new Pancake();
+            size++;
+            int pos = stack.length - size;
+            stack[pos] = p;
+        }
     }
 
-    public Pancake pop(){
-        if(isEmpty()) return null;
-        // remove a pancake from the stack
-        int pos = stack.length - size;
-        Pancake p = stack[pos];
-        stack[pos] = null;
-        size--;
-        return p;
-    }
-
+    public Pancake pop() {
+        if(!isEmpty()) {
+            int pos = stack.length - size;
+            Pancake p = stack[pos];
+            stack[pos] = null;
+            size--;
+            return p;
+        }
+        return null;
+    } 
+    
     public boolean isEmpty() {
         return size == 0;
     }
